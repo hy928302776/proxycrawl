2a31503f8ea1e83200a89e4f2c42287f23c47b9f
hy928302776 <hy928302776@github.com>
2023-09-10 02:13:34 +0800

    整理

--- core/StatsZxfbWeb.py
+++ core/StatsZxfbWeb.py
@@ -3,6 +3,9 @@ import sys
 from service.StatsZxfb import stats_zxfb
 
 if __name__ == '__main__':
-    beginTime = sys.argv[1]  # 开始时间
-    endTime = sys.argv[2]  # 结束时间"2023-08-28"
+    beginTime = None
+    endTime = None
+    if len(sys.argv) > 1:
+        beginTime = sys.argv[1]  # 开始时间
+        endTime = sys.argv[2]  # 结束时间"2023-08-28"
     stats_zxfb(beginTime, endTime, True)
