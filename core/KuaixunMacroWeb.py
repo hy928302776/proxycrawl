90c64c5210bb54f791f1a4f7470cf72beb4b515c
hy928302776 <hy928302776@github.com>
2023-09-10 23:31:04 +0800

    整理

---
+++ core/KuaixunMacroWeb.py
@@ -0,0 +1,11 @@
+import sys
+
+from service.KuaixunMacro import kuaixun_macro
+
+if __name__ == '__main__':
+    beginTime = None
+    endTime = None
+    if len(sys.argv) > 1:
+        beginTime = sys.argv[1]  # 开始时间
+        endTime = sys.argv[2]  # 结束时间"2023-08-28"
+    kuaixun_macro(beginTime, endTime, True)
