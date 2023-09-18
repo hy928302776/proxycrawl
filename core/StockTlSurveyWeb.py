c060c0b07c1066a5a02f3d5ccec6d84a69eda89c
hy928302776 <hy928302776@github.com>
2023-09-18 23:00:48 +0800

    sql优化

--- core/StockTlSurveyWeb.py
+++ core/StockTlSurveyWeb.py
@@ -7,8 +7,8 @@ from storage.MySqlStore import  MainDb
 
 if __name__ == "__main__":
     bMilvus = True if len(sys.argv) < 2 else sys.argv[1] == 'True'
-    start = None if len(sys.argv) < 3 else int(sys.argv[2])
-    offset = None if len(sys.argv) < 4 else int(sys.argv[3])
+    start = 0 if len(sys.argv) < 3 else int(sys.argv[2])
+    offset = 10000 if len(sys.argv) < 4 else int(sys.argv[3])
     beginTime = None if len(sys.argv) < 5 else sys.argv[4]  # 开始时间 "2023-08-27"
     endTime = None if len(sys.argv) < 6 else sys.argv[5]  # 结束时间"2023-08-28"
 
