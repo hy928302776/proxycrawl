8b33920787b08e8e3096509b76e666efd657e59a
hy928302776 <hy928302776@github.com>
2023-09-19 10:13:44 +0800

    sql优化

--- core/DataSynAifinIndustry.py
+++ core/DataSynAifinIndustry.py
@@ -4,6 +4,6 @@ import sys
 from service.DataSynAifin import data_sys_aifin
 
 if __name__ == '__main__':
-    status = 1 if len(sys.argv) < 2 else int(sys.argv[1])
+    status = 3 if len(sys.argv) < 2 else int(sys.argv[1])
     # 同步数据
     data_sys_aifin("aifin_industry", "aifin_industry", True, status)
--- core/DataSynAifinStock.py
+++ core/DataSynAifinStock.py
@@ -5,5 +5,5 @@ from service.DataSynAifin import data_sys_aifin
 
 if __name__ == '__main__':
     # 同步数据
-    status = 1 if len(sys.argv) < 2 else int(sys.argv[1])
+    status = 2 if len(sys.argv) < 2 else int(sys.argv[1])
     data_sys_aifin("aifin_stock", "aifin_stock", True, status)
